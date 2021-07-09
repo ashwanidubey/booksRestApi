03a76b4daf7ce38e769a5b7becd2ba04fef53528
Ashwani Dubey <ashwani786dubey@gmail.com>
2021-07-09 17:51:29 +0530

    add

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const cors=require("cors")
 const winston=require("winston")
 const app=express();
 require("dotenv").config();
-const port=process.env.PORT || 3000 ;
+const port=process.env.PORT || 8000 ;
 const booksRoute=require("./routes/books")
 
 //middleware
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -37,6 +39,9 @@ mongoose.connect(process.env.MONGO_URL,
 }).then(()=>{
    console.log("connection successfull")
 }).catch((e)=>{logger.log("error",e)})
+
+
+//listen port
 app.listen(port ,()=>{
     console.log(`server started a port ${port}`)
 })
